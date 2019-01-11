45b2ffc72421c26cae0037d3dfd2f1ca62d1563b
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-01-11 04:34:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,14 @@
 function forLoop(array){
   for (var i =0; i < 25; i++) {
     if( i === 1) {
-      console.log("I am " + i + " strange loop.")
+      array.push("I am " + i + " strange loop.");
     }else {
-      console.log(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`);
 
     }
 
-  }
-
+  } return array
+    
 }
 
 
